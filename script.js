ece12445f21e2724bb5fb8aa840920cf7861cfeb
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-18 14:31:14 +0530

    Everything except display of computer choice has been added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const gameOn = document.querySelector(".gameOn")
+const wannaPlay = document.querySelector('.wannaPlay')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const yes = document.querySelector(".yes");
+  yes.addEventListener('click', () => {
+    gameOn.style.display = "block"; 
+    game();
+    wannaPlay.style.display = "none";
+   });
+
+   const no = document.querySelector('.no');
+   no.addEventListener('click', () => window.close())
+});
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
@@ -33,7 +48,8 @@ function game() {
       const playerSelection = event.target.id; // get the id of the clicked button
       const computerSelection = getComputerChoice();
       const roundResult = playRound(playerSelection, computerSelection);
-      console.log(roundResult);
+      document.getElementById("who-won").innerHTML = roundResult;
+
 
       if (roundResult.startsWith("You Win")) {
         playerScore++;
@@ -41,20 +57,28 @@ function game() {
         computerScore++;
       }
 
-      console.log("Player Score: " + playerScore);
-      console.log("Computer Score: " + computerScore);
+      // console.log("Player Score: " + playerScore);
+      // console.log("Computer Score: " + computerScore);
+
+      document.getElementById("p-score").innerHTML = playerScore;
+      document.getElementById("c-score").innerHTML = computerScore;
 
       if (playerScore === 5) {
         console.log("You Win!");
+        document.getElementById("who-won").innerHTML = "You Win!"
+        gameOn.style.display = "none";
+        wannaPlay.style.display = "block";
         playerScore = 0;
         computerScore = 0;
       } else if (computerScore === 5) {
         console.log("You Lose!");
+        document.getElementById("who-won").innerHTML = "You Lose!"
+        gameOn.style.display = "none";
+        wannaPlay.style.display = "block";
         playerScore = 0;
         computerScore = 0;
       }
     });
   });
 }
-
 
